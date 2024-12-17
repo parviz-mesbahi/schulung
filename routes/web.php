7a87206a923625b45a2a86fc731a8a9f0d35b69b
parvis.mesbahi <parviz.mesbahi@chefkoch.de>
2024-12-17 15:26:40 +0100

    Generate different pages

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,28 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('under_construction');
+});
+
+Route::get('/landingpage', function () {
+    return view('landingpage');
+});
+
+Route::get('/links', function () {
+    return view('links');
+});
+
+Route::get('/impressum', function () {
+    return view('impressum');
+});
+
+Route::get('/datenschutz', function () {
+    return view('datenschutz');
 });
 
 Route::get('/dashboard', function () {
