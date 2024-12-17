1cf311e016c04dc08e0a5afd456905b4b6d2a79d
parvis.mesbahi <parviz.mesbahi@chefkoch.de>
2024-12-17 14:02:44 +0100

    Filament pages and files

---
+++ app/Filament/Resources/LessonResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LessonResource\Pages;
+use App\Filament\Resources\LessonResource\RelationManagers;
+use App\Models\Lesson;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LessonResource extends Resource
+{
+    protected static ?string $model = Lesson::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('number')
+                    ->required()
+                    ->label('Number'),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->unique()
+                    ->label('Title'),
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->label('Description'),
+//                Forms\Components\TextInput::make('content')
+//                ->required()
+//                ->label('Content'),
+                Forms\Components\MarkdownEditor::make('content')
+                    ->required()
+                    ->label('Content'),
+//                Forms\Components\RichEditor::make('content')
+//                    ->required()
+//                    ->label('Content')
+//                    ->placeholder('content here'),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('number'),
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('description'),
+//               Tables\Columns\TextColumn::make('content'),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLessons::route('/'),
+            'create' => Pages\CreateLesson::route('/create'),
+            'edit' => Pages\EditLesson::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/TaskResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TaskResource\Pages;
+use App\Filament\Resources\TaskResource\RelationManagers;
+use App\Models\Task;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TaskResource extends Resource
+{
+    protected static ?string $model = Task::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('lesson_id') // Add relationship dropdown
+                ->label('Related Lesson')
+//                    ->relationship('lesson', 'number')
+                    ->relationship('lesson', 'title')
+                    ->required(),
+                Forms\Components\TextInput::make('category')
+                    ->required()
+                    ->label('Category'),
+                Forms\Components\MarkdownEditor::make('question')
+                    ->required()
+                    ->label('Question'),
+                Forms\Components\MarkdownEditor::make('answer')
+                    ->required()
+                    ->label('Answer'),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('lesson_id'),
+                Tables\Columns\TextColumn::make('category'),
+                Tables\Columns\TextColumn::make('question'),
+                Tables\Columns\TextColumn::make('answer'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTasks::route('/'),
+            'create' => Pages\CreateTask::route('/create'),
+            'edit' => Pages\EditTask::route('/{record}/edit'),
+        ];
+    }
+}
