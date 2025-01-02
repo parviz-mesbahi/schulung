4c435b27aec9d2c2ca398a20b15640d08a7ceb97
parvis.mesbahi <parviz.mesbahi@chefkoch.de>
2025-01-02 15:14:04 +0100

    Home Texte ergänzt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -219,7 +219,11 @@
                         <img class="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm" src="https://maxabit-schulungen.de/image/Bild-Maxabit-Schulung-2.jpg" alt="">
                         <div class="w-full flex-auto">
                             <h2 class="text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">Der Inhalt der Schulungen</h2>
-                            <p class="mt-6 text-pretty text-lg/8 text-gray-300">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+                            <p class="mt-6 text-pretty text-lg/8 text-gray-300">
+                                Profitieren Sie von praxisorientierten Onlinekursen, die Ihnen fundiertes Wissen
+                                in Java, Algorithmen, Datenbanken und Spring Boot vermitteln. Jederzeit und
+                                flexibel.
+                            </p>
                             <ul role="list" class="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base/7 text-white sm:grid-cols-2">
                                 <li class="flex gap-x-3">
                                     <svg class="h-7 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
@@ -243,7 +247,7 @@
                                     <svg class="h-7 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                                         <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" />
                                     </svg>
-                                    Web Design
+                                    Beratung
                                 </li>
                                 <li class="flex gap-x-3">
                                     <svg class="h-7 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
@@ -255,7 +259,7 @@
                                     <svg class="h-7 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                                         <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" />
                                     </svg>
-                                    Bootstrap
+                                    Planung
                                 </li>
                             </ul>
                             <div class="mt-10 flex">
