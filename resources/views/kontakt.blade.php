f1683f715214180744fc7c54fdb5c9be044bdbe9
parvis.mesbahi <parviz.mesbahi@chefkoch.de>
2025-03-27 11:01:59 +0100

    Button Text geändert

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -166,7 +166,7 @@
                         und zu keinem anderen Zweck als die Kontaktaufnahme zu dir verwendet.
                     </div>
                     <div class="mt-8 flex justify-end">
-                        <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send message</button>
+                        <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Anfrage senden</button>
                     </div>
                 </div>
             </form>
